bde7533d31d6f11fcd938ad8154e797f2ce642f4
Vityauh <ziemann.karol@gmail.com>
2017-05-04 13:40:39 +0200

    actual quiz + id

---
+++ QuizGame/app/src/main/java/com/example/demon/quizgame/ActualQuizTaken.java
@@ -0,0 +1,13 @@
+package com.example.demon.quizgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActualQuizTaken extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actual_quiz_taken);
+    }
+}
